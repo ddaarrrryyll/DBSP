46f5f90b4b1f56e53782484b1de9b0ebad1ddf22
Shaun Yuen <shaunyuencw@gmail.com>
2023-09-09 18:42:28 +0800

    Some temp fixes

--- project1/storageComponent/Database.java
+++ project1/storageComponent/Database.java
@@ -5,18 +5,13 @@ package DBSP.project1.storageComponent;
 public class Database {
     private Block[] database = new Block[262144];
     
+    public Database(int diskCapacity, int blockSize) {
+        // initialise database
+        // initialise a new B+ tree
+        // initialise a new block
+    }
 
     // convert each row into a Record obj using record constructor for insertion
     // for block in database:
         // insert 16 records into block using block constructor
-}
-
-class Block {
-    private Record[] records = new Record[16];
-    // make constructor that takes in record array and set to this.records
-}
-
-public class Record {
-    // declare byte array for each column according to specs (date should be 4 bytes or less, rmb to stay within 25bytes)
-    // make constructor to take in the values in each column so Database obj can call
 }
